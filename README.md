794ac424145e65350c0f9c1561927342da3e3e4f
tabalamari <tabalamari@gmail.com>
2022-02-19 08:54:53 +0200

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# test-task_SEVEN
+How to run
+===
+1. npm i
+2. npm start
+
+task
+===
+on Angular
+
+1. Connect image cropper
+2. Save the crop image in localstorage in base64 format 128px at 128px
+3. UI is displayed at your discretion
---
+++ src/app/app.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { ImageCroppedEvent, LoadedImage } from 'ngx-image-cropper';
+
+@Component({
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+    imageChangedEvent: any = '';
+    croppedImage: any = '';
+
+    fileChangeEvent(event: any): void {
+        this.imageChangedEvent = event;
+    }
+    imageCropped(event: ImageCroppedEvent) {
+        this.croppedImage = event.base64;
+    }
+    imageLoaded(image: LoadedImage) {
+        // show cropper
+    }
+    cropperReady() {
+        // cropper ready
+    }
+    loadImageFailed() {
+        // show message
+    }
+}
